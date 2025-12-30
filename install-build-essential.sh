4516334dc31140b4a935c76a6ddbef92e47f5259
Riefe 24 <120639980+ThRE-Team@users.noreply.github.com>
2025-12-30 12:48:27 +0700

    Update install-build-essential.sh

--- install-build-essential.sh
+++ install-build-essential.sh
@@ -2,8 +2,8 @@
 
 sudo apt-get update -y
 sudo apt-get install wget curl aria2 -y
-sudo apt-get install build-essential g++-arm-linux-gnueabi g++-aarch64-linux-gnu bc -y
-sudo apt-get install clang
-sudo apt-get install llvm
+sudo apt-get install build-essential g++-arm-linux-gnueabi g++-aarch64-linux-gnu bc libssl-dev -y
+sudo apt-get install clang -y
+sudo apt-get install llvm -y
 
 # linex Project
